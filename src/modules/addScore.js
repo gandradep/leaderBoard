69e4d1c7cd3d63dee0608ea154185d4c5fa42dd4
gandradep <eventsonido@gmail.com>
2022-10-12 18:33:41 +0100

    Fixing linters

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -2,7 +2,10 @@
 export const addScore = (name, score) => fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0c1YpfazrDql64Q7vk4a/scores',
   {
     method: 'POST',
-    body: JSON.stringify({ user: name, score: score }),
+    body: JSON.stringify({
+      user: name,
+      score,
+    }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
